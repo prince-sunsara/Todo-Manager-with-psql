5cd8c378b196bef2e5c58c6f89d1124517df6492
prince-sunsara <princesunasara977@gmail.com>
2022-12-17 15:03:40 +0530

    Files updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
-const Todo = require("./models");
+const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -26,45 +26,45 @@ app.get("/todos", async function (_request, response) {
   }
 });
 
-app.get("/todos/:id", async function (request,response) {
-    try {
-        const todo = await Todo.findByPk(request.params.id);
-        return response.json(todo);
-    } catch (error) {
-        console.log(error);
-        return response.status(422).json(error);
-    }
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.post("/todos", async function (request, response) {
-    try {
-        const todo = await Todo.addTodo(request.body);
-        return response.json(todo);
-    } catch (error) {
-        console.log(error);
-        return response.status(422).json(error);
-    }
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
-    const todo = await Todo.findByPk(request.params.id);
-    try {
-        const updatedTodo = await todo.markAsCompleted();
-        return response.json(updatedTodo);  
-    } catch (error) {
-        return response.status(422).json(error);
-    }
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    return response.status(422).json(error);
+  }
 });
 
 app.delete("/todos/:id", async function (request, response) {
-    console.log("We have to delete a Todo with ID: ",request.params.id);
-    
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+
   // FILL IN YOUR CODE HERE
 
   // First, we have to query our database to delete a Todo by ID.
   const affectedRow = await Todo.destroy({ where: { id: request.params.id } });
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  response.send( affectedRow ? true : false);
+  response.send(affectedRow ? true : false);
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
 app.listen(3000, () => {
-    console.log("Starting express server at port 3000");
+  console.log("Starting express server at port 3000");
 });
